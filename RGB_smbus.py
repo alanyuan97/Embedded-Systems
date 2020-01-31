81887eac0071754799f9ccbf6924e8a1dba885e6
alanyuan97 <alanyuan97@gmail.com>
2020-01-31 21:05:44 +0000

    TODO fix bug

--- RGB_smbus.py
+++ RGB_smbus.py
@@ -13,7 +13,7 @@ i2c_address = 0x29
 # Register addresses
 reg_config = 0x0D
 # ******** CONTROL REG *************
-reg_control = 0x0F # CONTROL registers BIT7:0 RST:INT:GAIN*2:BANK:2:DATARDY:RSVD
+reg_control = 0x0F # CONTROL registers
 
 reg_status = 0x13 # LSB flag 
 # ********** SENSOR RAW DATA *************
@@ -26,61 +26,51 @@ reg_G_LOW = 0x18
 reg_R_HIGH = 0x17
 reg_R_LOW = 0x16
 
+reg_Data = 0x14
+
 
 # Functions definition
-def read_raw_data(channel):
-    if (channel == "R"):
-        reg_addr = reg_R_HIGH
-    elif (channel == "B"):
-        reg_addr = reg_B_HIGH
-    elif (channel == "G"):
-        reg_addr = reg_G_HIGH
-    else:
-        print("Invalid data reading. ")
-    bus.write_byte_data(i2c_address, 0x01, reg_addr)
 
-    while(True):
-        status = bus.read_byte_data(i2c_address,0x00)
-        if (status&0b00000001)==1:
-            break
-        else:
-            # print("-")
-            pass
-    data = bus.read_byte_data(i2c_address,0x02)
-    # data = val2[0]<<8 & val2[0]   
+def read_raw_data(address):
+    
+    data = bus.read_i2c_block_data(i2c_address,address,8)
+    # return a list of 8 bytes
     return data
+
 
 def on_message(client,userdata,message):
     print("Received message:{} on topic {}".format(message.payload,message.topic()))
 
-def CONFIG():
-    return bus.read_i2c_block_data(i2c_address, reg_config, 1)
+def GET_CONTROL():
+    """
+        Control Registers : Bits 7-2 are reserved
+                            Bits 1-0 are used for gain mode
+                            00: 1x
+                            01: 4x
+                            10: 16x
+                            11: 60x
 
-def set_mode(input):
-    pass
-def set_gain(gain):
-    REG_MASK = 0xFF
-    control_output = []
-    gain = gain << 4
-    control_input = CONFIG()
-    #print(f"CONFIG DATA: {control_input[0:]}")
-    print(f"CONFIG DATA: {bin(control_input[0])}")
-    control_output.append((control_input[0] & 0b11001111) | gain)
-    bus.write_i2c_block_data(i2c_address, reg_config, control_output)
-    print(f"CONTROL BITS: {bin(control_output[0]&REG_MASK)}")
-    control_input = CONFIG()
-    print(f"CONFIG DATA: {bin(control_input[0])}")
+    """
+    return bus.read_i2c_block_data(i2c_address, reg_control, 1)
+
+def set_gain(gain): # 0 for 1x, 1 for 4x, 2for 16x, 3 for 64x
+    REG = GET_CONTROL()
+    temp = [(REG[0] & 0xFC) + (gain & 0x11)]
+    bus.write_i2c_block_data(i2c_address, reg_control,temp)
 
 def init():
-    while True:
-        status = bus.read_byte_data(i2c_address, 0x00)
-        if (status&0b00000010)==0:
+    while(True):
+        status = bus.read_i2c_block_data(i2c_address,reg_status,1)
+        print(bin(status[0]))
+        if (status[0] & 0b1)==1:
+            print("Chip power on")
             break
         else:
             pass
+    bus.write_i2c_block_data(i2c_address,0x00,[0b11])
+    status = bus.read_i2c_block_data(i2c_address,0x00,1)
+    print(bin(status[0]))
     return
-
-    
 # Initialize I2C (SMBus)
 bus = smbus.SMBus(i2c_ch)
 client = mqtt.Client()
@@ -96,28 +86,27 @@ index=0
 try:
     # set_gain(0b11)
     init()
+    set_gain(0)
     while(True):
-        
+        data = read_raw_data(reg_Data)
+        print(data)
+        cData = data[1] * 256 + data[0]
+        red = data[3] * 256 + data[2]
+        green = data[5] * 256 + data[4]
+        blue = data[7] * 256 + data[6]
 
-        data = []
-        data.append(read_raw_data("V"))
-        data.append(read_raw_data("B"))
-        data.append(read_raw_data("G"))
-        data.append(read_raw_data("Y"))
-        data.append(read_raw_data("O"))
-        data.append(read_raw_data("R"))
-        print("\n V:{0} \n B:{1} \n G:{2} \n Y:{3} \n O:{4} \n R:{5}".format(data[0],data[1],data[2],data[3],data[4],data[5]))
+        print("\n C:{0} \n R:{1} \n G:{2} \n B:{3} ".format(cData,red,green,blue))
 
-        
+        newData=[]
+        newData.append(cData)
+        newData.append(red)
+        newData.append(green)
+        newData.append(blue)
         y = json.dumps(data)
         if (index==0):
             pub_success=client.publish("IC.embedded/Fat Duck/test",y)
             print(mqtt.error_string(pub_success.rc))
             index+=1
-
-    
-
-    
         time.sleep(0.5)
 except KeyboardInterrupt:
     quit()
