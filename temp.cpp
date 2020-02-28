a560380d732e62109b0937cfe1930d74157bff96
dominicyao97 <44146216+dominicyao97@users.noreply.github.com>
2020-02-28 18:05:05 +0000

    new ticker

--- temp.cpp
+++ temp.cpp
@@ -48,7 +48,7 @@ State   L1  L2  L3
 const int8_t driveTable[] = {0x12,0x18,0x09,0x21,0x24,0x06,0x00,0x00};
 
 //Mapping from interrupter inputs to sequential rotor states. 0x00 and 0x07 are not valid
-const int8_t stateMap[] = {0x07,0x05,0x03,0x04,0x01,0x00,0x02,0x07};  
+const int8_t stateMap[] = {0x07,0x05,0x03,0x04,0x01,0x00,0x02,0x07};
 //const int8_t stateMap[] = {0x07,0x01,0x03,0x02,0x05,0x00,0x04,0x07}; //Alternative if phase order of input or drive is reversed
 
 //Phase lead to make motor spin
@@ -56,19 +56,21 @@ const int8_t lead = 2;  //2 for forwards, -2 for backwards
 int8_t orState = 0;
 volatile uint32_t hash_counter=0;
 
+uint8_t sequence[] = {0x45,0x6D,0x62,0x65,0x64,0x64,0x65,0x64, 0x20,0x53,0x79,0x73,0x74,0x65,0x6D,0x73, 0x20,0x61,0x72,0x65,0x20,0x66,0x75,0x6E, 0x20,0x61,0x6E,0x64,0x20,0x64,0x6F,0x20, 0x61,0x77,0x65,0x73,0x6F,0x6D,0x65,0x20, 0x74,0x68,0x69,0x6E,0x67,0x73,0x21,0x20, 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00, 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+uint64_t* key = (uint64_t*)&sequence[48];
+uint64_t* nonce = (uint64_t*)&sequence[56];
+
+
 
 typedef struct{
-    uint32_t data; 
+    uint32_t data;
+    int typenames; //1 for nounce, 2 for hash rate
 }message;
 
 Mail<message, 16> mail_box;
-Thread thread;
+//Thread compute_thread;
+Thread print_thread;
 
-void putMessage(uint64_t* nonce){
-    message *mail = mail_box.alloc(); 
-    mail->data = *nonce;
-    mail_box.put(mail);
-}
 
 
 Serial pc(SERIAL_TX, SERIAL_RX);
@@ -93,10 +95,10 @@ PwmOut MotorPWM(PWMpin);
 
 //Set a given drive state
 void motorOut(int8_t driveState){
-    
+
     //Lookup the output byte from the drive state.
     int8_t driveOut = driveTable[driveState & 0x07];
-      
+
     //Turn off first
     if (~driveOut & 0x01) L1L = 0;
     if (~driveOut & 0x02) L1H = 1;
@@ -104,7 +106,7 @@ void motorOut(int8_t driveState){
     if (~driveOut & 0x08) L2H = 1;
     if (~driveOut & 0x10) L3L = 0;
     if (~driveOut & 0x20) L3H = 1;
-    
+
     //Then turn on
     if (driveOut & 0x01) L1L = 1;
     if (driveOut & 0x02) L1H = 0;
@@ -113,18 +115,18 @@ void motorOut(int8_t driveState){
     if (driveOut & 0x10) L3L = 1;
     if (driveOut & 0x20) L3H = 0;
 }
-    
+
     //Convert photointerrupter inputs to a rotor state
 inline int8_t readRotorState(){
     return stateMap[I1 + 2*I2 + 4*I3];
     }
 
-//Basic synchronisation routine    
+//Basic synchronisation routine
 int8_t motorHome(){
     //Put the motor in drive state 0 and wait for it to stabilise
     motorOut(0);
     wait(2.0);
-    
+
     //Get the rotor state
     return readRotorState();
 }
@@ -155,88 +157,79 @@ void CheckState(){
 }
 
 void CountHash(){
-    hash_counter = 0; 
+  message *mail = mail_box.alloc();
+  mail->data = hash_counter;
+  mail->typenames = 2;
+  mail_box.put(mail);
+  hash_counter = 0;
 }
 
-//void computeHash(){
-//    
-// if .... hash true:
-// -> putMessage()
-//}
 
-// void myprint(){
-// get mail ptr, &ptr, pc.print()
-//}
+void myprint(){
+    while (true){
+        osEvent evt = mail_box.get();
+        if (evt.status == osEventMail) {
 
-// thread.start(callback(myprint))
+            message *mail = (message*)evt.value.p;
+            pc.printf("\ntype: 0x%x\n\r", mail->typenames);
+            pc.printf("\ndata: 0x%x\n\r", mail->data);
+            mail_box.free(mail);
+        }
+    }
+}
 
 
-// in main:
-//      while(1):
-//          computeHash()
+void compute(){
+    uint8_t hash1[32];
+    SHA256::computeHash(hash1, sequence, 64);
+    if ((hash1[0]==0) && (hash1[1]==0)){
+            message *mail = mail_box.alloc();
+            mail->data = *nonce;
+            mail->typenames = 1;
+            mail_box.put(mail);
+        }
 
+}
 
 //Main
 int main() {
     // int8_t orState = 0;    //Rotot offset at motor state 0
     // int8_t intState = 0;
     // int8_t intStateOld = 0;
-    
-    uint8_t sequence[] = {0x45,0x6D,0x62,0x65,0x64,0x64,0x65,0x64, 0x20,0x53,0x79,0x73,0x74,0x65,0x6D,0x73, 0x20,0x61,0x72,0x65,0x20,0x66,0x75,0x6E, 0x20,0x61,0x6E,0x64,0x20,0x64,0x6F,0x20, 0x61,0x77,0x65,0x73,0x6F,0x6D,0x65,0x20, 0x74,0x68,0x69,0x6E,0x67,0x73,0x21,0x20, 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00, 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
-    uint64_t* key = (uint64_t*)&sequence[48]; 
-    uint64_t* nonce = (uint64_t*)&sequence[56]; 
-    uint8_t hash1[32];
+
     Ticker timing;
 
 
     const int32_t PWM_PRD = 2500;
     MotorPWM.period_us(PWM_PRD);
     MotorPWM.pulsewidth_us(PWM_PRD);
-    
+
     //Initialise the serial port
     pc.printf("Hello\n\r");
-    
+
     //Run the motor synchronisation
     orState = motorHome();
     pc.printf("Rotor origin: %x\n\r",orState);
     //orState is subtracted from future rotor state inputs to align rotor and motor states
-    
+
     MotorPWM.pulsewidth_us(PWM_PRD/2);
     //Poll the rotor state and set the motor outputs accordingly to spin the motor
-    
+
     timing.attach(&CountHash, 1.0);
 
     CheckState();
-    thread.start(callback(nonce, putMessage));
+    //compute_thread.start(callback(compute));
+    print_thread.start(callback(myprint));
     while (1) {
-        // intState = readRotorState();
-        // if (intState != intStateOld) {
-        //     motorOut((intState-orState+lead+6)%6); //+6 to make sure the remainder is positive
-        //     if (intState == 4 && intStateOld == 3) TP1 = !TP1;
-        //     intStateOld = intState;
-        //     pc.printf("%d\n\r",intState);
-        // }
-        SHA256::computeHash(hash1, sequence, 64);
-        osEvent evt = mail_box.get();
-        if (evt.status == osEventMail) {
-            pc.printf("hh");
-            if ((hash1[0]==0) && (hash1[1]==0)){
-                pc.printf("aa");
-            message *mail = (message*)evt.value.p;
-            pc.printf("\nnonce: 0x%x\n\r", mail->data);
-            mail_box.free(mail);
-            }
-        }
-
-
-        // if ((hash1[0]==0) && (hash1[1]==0)) {
-        // TODO print NONCE, *nonce first -> hex()
-        // pc.printf("hahahah");
-        // pc.printf("nounce is 0x%x\r\n",*nonce);
+         compute();
         *nonce = *nonce + 1;
-        hash_counter+=1; 
+         hash_counter+=1;
+
+
+
+
     }
+
+
 }
-
-
 
