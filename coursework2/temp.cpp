f9f339def7ff1dbcf2d42ab4f1bcb7ae81cffe37
alan <36449601+alanyuan97@users.noreply.github.com>
2020-03-03 16:29:38 +0000

    Refine print

--- coursework2/temp.cpp
+++ coursework2/temp.cpp
@@ -192,9 +192,25 @@ void myprint(){
         osEvent evt = mail_box.get();
         if (evt.status == osEventMail) {
             message *mail = (message*)evt.value.p;
-            pc.printf("\ntype: 0x%d\n\r", mail->typenames);
-            pc.printf("\ndata: 0x%d\n\r", mail->data);
-            mail_box.free(mail);
+
+            switch(mail->typenames){
+                // Type 1: Found Hash, Print nonce
+                case(1):
+                    pc.printf("\n Found Hash_nonce: 0x%x\n\r", mail->data);
+                    break;
+                // Type 2: Hash rate    
+                case(2):
+                    pc.printf("\n Hash-rate: %d\n\r", mail->data);
+                    break;
+                // Type 3: Abs positon of rotor 
+                case(3):
+                    pc.printf("\n Position: %d\n\r", mail->data);
+                    break;
+            }
+            // pc.printf("\ntype: 0x%d\n\r", mail->typenames);
+            // pc.printf("\ndata: 0x%d\n\r", mail->data);
+
+        mail_box.free(mail);
         }
     }
 }
